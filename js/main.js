04f345bc1c5537ca72df59ef389ac171cba2e30b
David <davidmenendezblanco@alumnos.tuniversformacion.es>
2023-11-30 18:59:16 +0100

    Estilado del contenedor y animaciones

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ const boton_limpiar = document.querySelector(".boton-limpiar");
 
 const tareaManager = new TareasManager(lista_tareas);
 
-//Iniciamos la app web
 tareaManager.inicializarContador();
 tareaManager.listarTareas();
 
@@ -18,17 +17,17 @@ boton_limpiar.addEventListener("click", () => {
   tareaManager.limpiarTodo();
 });
 
-lista_tareas.addEventListener("click", (event) => {
-  const target = event.target;
+lista_tareas.addEventListener("click", (e) => {
+  const target = e.target;
   if (target.type === "submit") {
     tareaManager.eliminarTarea(target.parentElement.id);
   }
 });
 
-lista_tareas.addEventListener("keypress", (event) => {
+lista_tareas.addEventListener("keypress", (e) => {
     const target = event.target;
-    //Alternativa al keyup enter, codigo ascii, 13 corrsponde a la tecla enter
-    if(event.keyCode === 13){
+    //Codigo ASCII, 13 corresponde a la tecla enter
+    if(e.keyCode === 13){
         tareaManager.editarTarea(target.parentElement.id, target.value);
     }
 });
--- js/tareasManager.js
+++ js/tareasManager.js
@@ -1,5 +1,6 @@
 import { Tarea } from "./tarea.js";
 
+
 export class TareasManager {
   constructor(listaTareas) {
     this.arrayTareas = [];
@@ -7,30 +8,38 @@ export class TareasManager {
     this.listaTareas = listaTareas;
   }
 
+  
   agregarTarea(descripcion) {
     this.contador++;
     const nuevaTarea = new Tarea(this.contador, descripcion);
     this.arrayTareas.push(nuevaTarea);
-    //Estas son para añadirlo al localStorage
+    
     this.setContador();
     this.setArrayTareas();
   }
 
   listarTareas() {
-    //Es para inicializar la descripcion de la tarea. Esta en blanco porque aun no la hemos añadido
     this.listaTareas.innerHTML = "";
+    // Si hay información en el localStorage, la carga
+    if(localStorage.getItem('arrayTareas') != null) {
+        this.arrayTareas = this.getArrayTareas();
+    }
     this.arrayTareas.reverse().forEach((tarea) => {
       this.listaTareas.innerHTML += `
-                <li id="${tarea.id}">
+                <li class="animated fadeIn" id="${tarea.id}">
                     <input type="text" class="input-tarea" value="${tarea.descripcion}" placeholder="Tarea nueva...">
                     <button class="boton-eliminar">X</button>
                 </li>
             `;
+
+            /* setTimeout(() => {
+                document.getElementById(tarea.id).classList.remove('animated', 'fadeIn');
+            }, 1000); */
     });
   }
 
   editarTarea(idTarea, descripcion) {
-    const tarea = this.arrayTareas.find((t) => (t.id = idTarea));
+    const tarea = this.arrayTareas.find((t) => (t.id == idTarea));
     if(tarea){
         tarea.editar(descripcion);
         this.setArrayTareas();
@@ -67,11 +76,17 @@ export class TareasManager {
   getArrayTareas(){
     this.setContador();
     const array = JSON.parse(localStorage.getItem('arrayTareas'));
-    return array || [];
+    // Convertir objetos genéricos en instancias de Tarea
+    const tareasConvertidas = array.map((tarea) => new Tarea(tarea.id, tarea.descripcion));
+    // Se puede hacer en una sola línea
+   /*  JSON.parse(localStorage.getItem('arrayTareas')).map((tarea) => new Tarea(tarea.id, tarea.descripcion)); */
+    return tareasConvertidas || [];
   }
 
   setArrayTareas(){
     localStorage.setItem('arrayTareas', JSON.stringify(this.arrayTareas));
+
+
     this.listarTareas();
   }
 
