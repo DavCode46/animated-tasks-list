6bfddd21e40fc9c7a73efc5feb6d4c229ebb9877
David <davidmenendezblanco@alumnos.tuniversformacion.es>
2023-11-30 19:16:40 +0100

    Modificado README y añadido favicon

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # Lista de Tareas con LocalStorage
 
-**Autor**: David Menéndez Blanco (Estudiante 2º DAW)<br>
-1. DWEC.
-2. DIW
+**Autor**: ***David Menéndez Blanco***
+<hr>
+
+## Tecnologías utilizadas:
+
+
+
+1. HTML
+2. CSS
+3. JS
--- js/tareasManager.js
+++ js/tareasManager.js
@@ -1,14 +1,24 @@
 import { Tarea } from "./tarea.js";
 
 
+/**
+ * Clase que representa un administrador de tareas.
+ */
 export class TareasManager {
+  /**
+   * Constructor de la clase TareasManager.
+   * @param {HTMLElement} listaTareas - Elemento HTML donde se mostrarán las tareas.
+   */
   constructor(listaTareas) {
     this.arrayTareas = [];
     this.contador = 0;
     this.listaTareas = listaTareas;
   }
 
-  
+  /**
+   * Agrega una nueva tarea al administrador de tareas.
+   * @param {string} descripcion - Descripción de la tarea.
+   */
   agregarTarea(descripcion) {
     this.contador++;
     const nuevaTarea = new Tarea(this.contador, descripcion);
@@ -18,6 +28,9 @@ export class TareasManager {
     this.setArrayTareas();
   }
 
+  /**
+   * Lista todas las tareas en el elemento HTML especificado.
+   */
   listarTareas() {
     this.listaTareas.innerHTML = "";
     // Si hay información en el localStorage, la carga
@@ -38,6 +51,11 @@ export class TareasManager {
     });
   }
 
+  /**
+   * Edita la descripción de una tarea existente.
+   * @param {number} idTarea - ID de la tarea a editar.
+   * @param {string} descripcion - Nueva descripción de la tarea.
+   */
   editarTarea(idTarea, descripcion) {
     const tarea = this.arrayTareas.find((t) => (t.id == idTarea));
     if(tarea){
@@ -46,11 +64,18 @@ export class TareasManager {
     }
   }
 
+  /**
+   * Elimina una tarea del administrador de tareas.
+   * @param {number} idTarea - ID de la tarea a eliminar.
+   */
   eliminarTarea(idTarea){
     this.arrayTareas = this.arrayTareas.filter((t) => (t.id != idTarea));
     this.setArrayTareas();
   }
 
+  /**
+   * Limpia todas las tareas del administrador.
+   */
   limpiarTodo(){
     this.arrayTareas = [];
     this.contador = 0;
@@ -58,21 +83,35 @@ export class TareasManager {
     this.setContador();
   }
 
+  /**
+   * Obtiene el valor del contador almacenado en el localStorage.
+   * @returns {number} - Valor del contador.
+   */
   getContador(){
     const cont = localStorage.getItem('contador');
     return cont;
   }
 
+  /**
+   * Almacena el valor del contador en el localStorage.
+   */
   setContador(){
     localStorage.setItem('contador',this.contador);
   }
 
+  /**
+   * Inicializa el contador con el valor almacenado en el localStorage.
+   */
   inicializarContador(){
     if(this.getContador() != null) {
         this.contador = this.getContador();
     }
   }
 
+  /**
+   * Obtiene el array de tareas almacenado en el localStorage.
+   * @returns {Array} - Array de tareas.
+   */
   getArrayTareas(){
     this.setContador();
     const array = JSON.parse(localStorage.getItem('arrayTareas'));
@@ -83,11 +122,11 @@ export class TareasManager {
     return tareasConvertidas || [];
   }
 
+  /**
+   * Almacena el array de tareas en el localStorage y actualiza la lista de tareas.
+   */
   setArrayTareas(){
     localStorage.setItem('arrayTareas', JSON.stringify(this.arrayTareas));
-
-
     this.listarTareas();
   }
-
 }
